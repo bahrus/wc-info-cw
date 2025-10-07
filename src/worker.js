13003c898ff293d762f6c86930588683d587d85f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-10-06 21:26:46 -0400

    Initial commit

--- src/worker.js
+++ src/worker.js
@@ -89,7 +89,7 @@ export default {
     </head>
     <body>
     <header class="package-header" part="package-header" itemscope itemtype="https://cdn.jsdelivr.net/npm/custom-elements-manifest@1.0.0/schema.json#definitions/Reference">
-      <button popovertarget=navigation-popup>TOC</button>
+      <button popovertarget=navigation-popup>&#9776;</button>
       <dialog popover=auto id=navigation-popup>
         <nav id=nav onclick=parentElement.hidePopover()></nav>
       </dialog>  
